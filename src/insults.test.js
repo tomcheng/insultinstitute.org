34ad9eb46b950e411f13c8c92c7ecb793b2052a8
Thomas Cheng <thomascheng81@gmail.com>
2017-01-26 23:55:26 -0500

    Add tests to verify buckets

--- src/insults.test.js
+++ src/insults.test.js
@@ -1,38 +1,19 @@
-if (!Array.prototype.includes) {
-  Object.defineProperty(Array.prototype, 'includes', {
-    value: function(searchElement, fromIndex) {
-      if (this == null) { throw new TypeError('"this" is null or not defined'); }
-
-      var o = Object(this);
-      var len = o.length >>> 0;
-
-      if (len === 0) { return false; }
-
-      var n = fromIndex | 0;
-      var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
-
-      while (k < len) {
-        if (o[k] === searchElement) { return true; }
-        k++;
-      }
-
-      return false;
-    }
-  });
-}
-
 import { applyModifier } from "./insults";
 
-it("pluralizes", () => {
-  expect(applyModifier("foo", "p")).toBe("foos");
-  expect(applyModifier("fish", "p")).toBe("fishes");
-  expect(applyModifier("case", "p")).toBe("cases");
+it("capitalizes", () => {
+  expect(applyModifier("foo", "c")).toBe("Foo");
 });
 
 it("adds an indefinite article", () => {
   expect(applyModifier("foo", "s")).toBe("a foo");
   expect(applyModifier("ant", "s")).toBe("an ant");
   expect(applyModifier("use", "s")).toBe("a use");
+});
+
+it("pluralizes", () => {
+  expect(applyModifier("foo", "p")).toBe("foos");
+  expect(applyModifier("fish", "p")).toBe("fishes");
+  expect(applyModifier("case", "p")).toBe("cases");
 });
 
 it("turns words to doer nouns", () => {
