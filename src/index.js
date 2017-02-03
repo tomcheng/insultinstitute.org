ab0ea35757f4bf962bc835e625fe8407b3b9d138
Thomas Cheng <thomascheng81@gmail.com>
2017-02-02 23:16:09 -0500

    Add a face button

--- src/index.js
+++ src/index.js
@@ -3,22 +3,8 @@ import getInsult from "./insults";
 const insultContainer = document.getElementById("insult");
 const moreButton = document.getElementById("more");
 
-const BUTTON_TEXTS = [
-  "Show Me Another Insult",
-  "I Needs It",
-  "Hit Me Again",
-  "Again!",
-  "More!",
-  "Give It To Me",
-  "Tell Me More",
-];
-
-let buttonIndex = 0;
-
 const showInsult = () => {
   insultContainer.textContent = getInsult();
-  moreButton.textContent = BUTTON_TEXTS[buttonIndex % BUTTON_TEXTS.length];
-  buttonIndex++;
 };
 
 showInsult();
